4c6aa2163af368170a3c3e9f5a21c3309a4696e2
Yung chieh Tsai <x01ep23i@hotmail.com>
2017-07-31 23:40:03 +0800

    Update ehtereum wallet to generate private key

--- src/Upload.js
+++ src/Upload.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import PrivateKey from './PrivateKey'
+import EthWallet from 'ethereumjs-wallet'
 
 class Upload extends React.Component {
   state: {
     privateKey: string,
+    keystore: string,
+    passphrase: string,
   }
 
   // 建構式
@@ -14,12 +17,12 @@ class Upload extends React.Component {
     // 設定初始的狀態。注意！這裡有個反樣式，不要用props的值來設定state的初始值
     this.state = {
       privateKey: '',
+      keystore: '',
+      passphrase: '',
     }
   }
 
-  handleChange = (e: Event) => {
-    // Flow會檢查必定要HTMLInputElement的物件才能有輸入值
-    console.log(e);
+  handleUpload = (e: Event) => {
     if (e.target instanceof HTMLInputElement) {
       const file = e.target.files[0]
       let reader = new FileReader()
@@ -27,22 +30,44 @@ class Upload extends React.Component {
       reader.onload = (function(file){
         return function(event){
           const base64data = event.target.result.split(",")[1];
-          const privateKey = atob(base64data);
+          const json = atob(base64data);
+
+          // using ehtereumjs-wallet to private key
+          const wallet = EthWallet.fromV3(json, "OGRiMDdmNmU1YmUwNGUzODA0ODQ4MTc2", true);
+          const privateKey = wallet.getPrivateKeyString();
+          // 可以比對 MyEtherWallet -> View Wallet Info
+
           react.setState({
-            privateKey,
+            "keystore": json,
           })
+          
         };
       })(file);
       reader.readAsDataURL(file)
     }
   }
 
+  handlePassphrase = (e: Event) => {
+    if (e.target instanceof HTMLInputElement) {
+      this.setState({passphrase: e.target.value})
+    }
+  }
+
+  handleChange = (e: Event) => {
+    // Flow會檢查必定要HTMLInputElement的物件才能有輸入值
+    const wallet = EthWallet.fromV3(this.state.keystore, this.state.passphrase, true)
+    const privateKey = wallet.getPrivateKeyString()
+    this.setState({
+      privateKey,
+    })
+  }
+
   render() {
     return (
       <div>
-        <input type="file"
-          onChange={this.handleChange}
-        />
+        <input type="file" onChange={this.handleUpload}/>
+        <input type="text" placeholder="passphrase" onChange={this.handlePassphrase}/>
+        <button onClick={this.handleChange}>SUBMIT</button>
         <PrivateKey text={this.state.privateKey}/>
       </div>
     )
